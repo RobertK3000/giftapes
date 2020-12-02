452a266fe3e8bd5347467f81c778a529c0106106
Susan Pan <susan.pan96@gmail.com>
2020-12-02 13:37:18 +1000

    ARCH: collage_items_controller create

--- app/controllers/collage_items_controller.rb
+++ app/controllers/collage_items_controller.rb
@@ -1,6 +1,22 @@
 class CollageItemsController < ApplicationController
+  before_action :set_collage, only: [:new, :create]
   def new
-    @collage = Collage.find(params[:collage_id])
     @collage_item = CollageItem.new
+  end
+
+  def create
+    @collage_item = CollageItem.new()
+    @collage_item.collage = @collage
+    # if @collage.save
+
+    # else
+
+    # end
+  end
+
+  private
+
+  def set_collage
+    @collage = Collage.find(params[:collage_id])
   end
 end
