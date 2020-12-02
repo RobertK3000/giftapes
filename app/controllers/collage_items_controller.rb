47542fa2befef44a3c1095731f31a52763166133
Susan Pan <susan.pan96@gmail.com>
2020-12-02 13:34:51 +1000

    ARCH: collage_items_controller new

--- app/controllers/collage_items_controller.rb
+++ app/controllers/collage_items_controller.rb
@@ -1,3 +1,6 @@
 class CollageItemsController < ApplicationController
-
+  def new
+    @collage = Collage.find(params[:collage_id])
+    @collage_item = CollageItem.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :collages, only: [:new, :create, :show]
+  resources :collages, only: [:new, :create, :show] do
+    resources :collage_items, only: [:new, :create]
+  end
 end
