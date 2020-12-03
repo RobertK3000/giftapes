924414f68ede2d2c5f06932237805785a941e6a2
Ondine Charlesworth <ondinecharlesworth@gmail.com>
2020-12-03 13:00:07 +1100

    FIX: validations with and between curated lists and curated list items

--- app/models/curated_list.rb
+++ app/models/curated_list.rb
@@ -2,6 +2,6 @@ class CuratedList < ApplicationRecord
 
   has_many :curated_list_items
   has_one :giftape, as: :giftable
-
+  validates :name, presence: true
 
 end
--- app/models/curated_list_item.rb
+++ app/models/curated_list_item.rb
@@ -1,5 +1,6 @@
 class CuratedListItem < ApplicationRecord
 
-  belongs_to :curated_list
-
+  belongs_to :curated_list, foreign_key: "curated_list_id"
+  validates :prompt, presence: true
+  validates :url, presence: true
 end
