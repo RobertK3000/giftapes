153fe287c038424056521e8521c089f08ea6fbb0
Susan Pan <susan.pan96@gmail.com>
2020-12-04 10:51:31 +1000

    Tried some giphy js but will do later

---
+++ app/javascript/components/giphy.js
@@ -0,0 +1,10 @@
+// import { GiphyFetch } from '@giphy/js-fetch-api'
+// import { renderGrid } from '@giphy/js-components'
+
+// const gf = new GiphyFetch('tqAY8yMFBNvJKrB6Zhw4QOzXa4zZf0TN');
+
+// const { data: gifs } = await gf.trending({ limit: 10 });
+
+const results = document.querySelector("#giphy-results");
+const form = document.querySelector("#giphy-search-form");
+const query = document.querySelector("#giphy-search-query");
