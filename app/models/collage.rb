e1b1c188c7ddaf1f3c5283c4067797040231dc4b
Susan Pan <susan.pan96@gmail.com>
2020-12-06 22:35:54 +1000

    FIX: trying to fix collage javascript first line

--- app/models/collage.rb
+++ app/models/collage.rb
@@ -1,9 +1,7 @@
 class Collage < ApplicationRecord
   has_many :collage_items, inverse_of: :collage, dependent: :destroy
   has_one :giftape, as: :giftable
-  accepts_nested_attributes_for :collage_items, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :collage_items, allow_destroy: true
 
   validates :name, presence: :true
-  validates_presence_of :collage_items, acceptance: {message: 'Must have at least one photo'}
-  validates_associated :collage_items, acceptance: {message: 'Must have at least one photo'}
 end
