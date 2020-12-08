87aaad4cc0fc77b6c0ccff252455550d06709a82
Ondine Charlesworth <ondinecharlesworth@gmail.com>
2020-12-08 16:19:51 +1100

    FIX: minor bugs

--- app/models/collage.rb
+++ app/models/collage.rb
@@ -1,6 +1,6 @@
 class Collage < ApplicationRecord
   has_many :collage_items, inverse_of: :collage, dependent: :destroy
-  has_one :giftape, as: :giftable
+  has_one :giftape, as: :giftable, dependent: :destroy
   accepts_nested_attributes_for :collage_items, allow_destroy: true
   # validates :name, presence: :true
 end
--- app/models/curated_list.rb
+++ app/models/curated_list.rb
@@ -1,7 +1,7 @@
 class CuratedList < ApplicationRecord
 
   has_many :curated_list_items, dependent: :destroy
-  has_one :giftape, as: :giftable
+  has_one :giftape, as: :giftable, dependent: :destroy
   validates :name, presence: true
 
 end
